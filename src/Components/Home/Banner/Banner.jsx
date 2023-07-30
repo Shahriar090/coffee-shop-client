abcfbbe899d282cb87177a98c41667bcfb63cdc4
Shahriar090 <shahriarh655@gmail.com>
2023-07-30 12:15:03 +0600

    added some gradients to the banner images

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -10,8 +10,24 @@ const Banner = () => {
         {/* slide 01 */}
         <div id="slide1" className="carousel-item relative w-full">
           <img src={bannerImageOne} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">
+          <div className="absolute flex flex-col  top-1/2 left-1/2 bg-gradient-to-r p-16 rounded-full from-black to-transparent">
+            <h1 className="text-6xl text-white font-bold font-serif border-b-2 m-2">
+              Coffee Heaven
+            </h1>
+
+            <p className="text-white m-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Asperiores dolorum pariatur reprehenderit dignissimos. Tempore,
+              dolorum?
+            </p>
+            <div>
+              <button className="btn btn-outline text-white hover:bg-red-950 m-2">
+                Shop Here
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 bottom-0 ">
+            <a href="#slide3" className="btn btn-circle">
               ❮
             </a>
             <a href="#slide2" className="btn btn-circle">
@@ -22,7 +38,23 @@ const Banner = () => {
         {/* slide 02 */}
         <div id="slide2" className="carousel-item relative w-full">
           <img src={bannerImageTwo} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <div className="absolute flex flex-col  top-1/4 left-1/2 bg-gradient-to-r p-16 rounded-full from-black to-transparent">
+            <h1 className="text-6xl text-white font-bold font-serif border-b-2 m-2">
+              Coffee Factory
+            </h1>
+
+            <p className="text-white m-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Asperiores dolorum pariatur reprehenderit dignissimos. Tempore,
+              dolorum?
+            </p>
+            <div>
+              <button className="btn btn-outline text-white hover:bg-red-950 m-2">
+                Shop Here
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 bottom-0">
             <a href="#slide1" className="btn btn-circle">
               ❮
             </a>
@@ -34,11 +66,27 @@ const Banner = () => {
         {/* slide 03 */}
         <div id="slide3" className="carousel-item relative w-full">
           <img src={bannerImageThree} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <div className="absolute flex flex-col  top-20 left-14 bg-gradient-to-r p-16 rounded-full from-black to-transparent">
+            <h1 className="text-6xl text-white font-bold font-serif border-b-2 m-2">
+              Coffee Masters
+            </h1>
+
+            <p className="text-white m-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Asperiores dolorum pariatur reprehenderit dignissimos. Tempore,
+              dolorum?
+            </p>
+            <div>
+              <button className="btn btn-outline text-white hover:bg-red-950 m-2">
+                Shop Here
+              </button>
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 bottom-0">
             <a href="#slide2" className="btn btn-circle">
               ❮
             </a>
-            <a href="#slide4" className="btn btn-circle">
+            <a href="#slide1" className="btn btn-circle">
               ❯
             </a>
           </div>
